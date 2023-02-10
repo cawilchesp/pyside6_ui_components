c5f9e217a20803ab771bcc1f2ecadf32670651bd
Carlos Andrés Wilches Pérez <cawilchesp@outlook.com>
2023-02-10 16:28:32 -0500

    Update md3_datepicker.py

--- components/md3_datepicker.py
+++ components/md3_datepicker.py
@@ -20,18 +20,23 @@ class MD3DatePicker(QtWidgets.QFrame):
 
         Parameters
         ----------
-        geometry: tuple
-            Date field position and width
-            (x, y, w) -> x, y: upper left corner, w: width
-        labels: tuple
-            Date field text
-            (label_es, label_en) -> label_es: label in spanish, label_en: label in english
-        theme: bool
-            App theme
-            True: Light theme, False: Dark theme
-        language: int
-            App language
-            0: Spanish, 1: English
+        attributes: dict
+            name: str
+                Widget name
+            position: tuple
+                Button position
+                (x, y) -> x, y: upper left corner
+            width: tuple
+                Button width
+            labels: tuple
+                Date field text
+                (label_es, label_en) -> label_es: label in spanish, label_en: label in english
+            theme: bool
+                App theme
+                True: Light theme, False: Dark theme
+            language: int
+                App language
+                0: Spanish, 1: English
         
         Returns
         -------
@@ -61,16 +66,24 @@ class MD3DatePicker(QtWidgets.QFrame):
         self.label_field.setGeometry(8, 0, 16, 16)
         self.label_field.setFont(QtGui.QFont('Segoe UI', 9))
 
-        self.apply_styleSheet(attributes['theme'])
-        self.language_text(attributes['language'])
+        self.setThemeStyle(attributes['theme'])
+        self.setLanguage(attributes['language'])
 
 
-    def apply_styleSheet(self, theme: bool) -> None:
+    def setThemeStyle(self, theme: bool) -> None:
         """ Apply theme style sheet to component """
 
-        background_color = colors(theme, 'transparent_background')
-        color = light["on_surface"]
-        drop_background_color = light["primary"]
+        if self.parent.attributes['type'] == 'filled':
+            background_color = colors(theme, 'surface_tint')
+        elif self.parent.attributes['type'] == 'outlined':
+            background_color = colors(theme, 'background')
+        color = colors(theme, 'on_surface')
+        drop_background_color = colors(theme, 'primary')
+
+        if theme: icon_theme = 'L'
+        else: icon_theme = 'D'
+        current_path = sys.path[0].replace("\\","/")
+        images_path = f'{current_path}/icons'
 
         self.setStyleSheet(f'QFrame {{ '
                 f'background-color: {background_color} }}'
@@ -90,7 +103,8 @@ class MD3DatePicker(QtWidgets.QFrame):
                 f'left: -4;'
                 f'border-radius: 16 }}'
                 
-                f'QDateEdit::down-arrow {{ image: url({images_path}/calendar_L.png);'
+                f'QDateEdit::down-arrow {{ '
+                f'image: url({images_path}/calendar_{icon_theme}.png);'
                 f'width: 16; height: 16 }}'
                 
                 f'QLabel {{ border: 0px solid; '
@@ -98,7 +112,7 @@ class MD3DatePicker(QtWidgets.QFrame):
                 f'background-color: {background_color}; '
                 f'color: {color} }}')
 
-    def language_text(self, language: int) -> None:
+    def setLanguage(self, language: int) -> None:
         """ Change language of label text """
         if language == 0:   self.label_field.setText(self.attributes['labels'][0])
         elif language == 1: self.label_field.setText(self.attributes['labels'][1])
