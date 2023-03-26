08505bd4549c7321cb96400d46b05190e479103b
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-03-26 14:58:36 -0500

    Update md3_menu.py
    Change to PySide6

--- components/md3_menu.py
+++ components/md3_menu.py
@@ -1,11 +1,11 @@
 """
-PyQt Menu component adapted to follow Material Design 3 guidelines
+PySide6 Menu component adapted to follow Material Design 3 guidelines
 
 
 """
 
-from PyQt6 import QtWidgets
-from PyQt6.QtCore import Qt
+from PySide6 import QtWidgets
+from PySide6.QtCore import Qt
 
 from components.style_color import colors
 
