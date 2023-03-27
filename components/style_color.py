0e1e3d01e5ce33c988b2961e326a693f9ab3940b
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-03-27 14:28:52 -0500

    Update style_color.py

--- components/style_color.py
+++ components/style_color.py
@@ -1,34 +1,3 @@
-# :root {
-#   Styleguide Nord - Polar Night
-  
-#   --nord0: #2e3440;
-#   --nord1: #3b4252;
-#   --nord2: #434c5e;
-#   --nord3: #4c566a;
-
-#   Styleguide Nord - Snow Storm
-  
-#   --nord4: #d8dee9;
-#   --nord5: #e5e9f0;
-#   --nord6: #eceff4;
-
-#   Styleguide Nord - Frost
-  
-#   --nord7: #8fbcbb;
-#   --nord8: #88c0d0;
-#   --nord9: #81a1c1;
-#   --nord10: #5e81ac;
-  
-#   Styleguide Nord - Aurora
-  
-#   --nord11: #bf616a;
-#   --nord12: #d08770;
-#   --nord13: #ebcb8b;
-#   --nord14: #a3be8c;
-#   --nord15: #b48ead;
-# }
-
-
 def colors(theme: bool, role: str) -> str:
     light = {
         'background': '#eceff4',
@@ -36,8 +5,6 @@ def colors(theme: bool, role: str) -> str:
         'surface_tint': '#d8dee9',
         'surface': '#eceff4',
         'on_surface': '#001f2a',
-        'surface_variant': '#dfe2eb',
-        'on_surface_variant': '#43474e',
 
         'outline': '#2e3440',
 
@@ -62,8 +29,6 @@ def colors(theme: bool, role: str) -> str:
         'surface_tint': '#4c566a',
         'surface': '#2e3440',
         'on_surface': '#bfe9ff',
-        'surface_variant': '#43474e',
-        'on_surface_variant': '#c3c6cf',
 
         'outline': '#d8dee9',
 
