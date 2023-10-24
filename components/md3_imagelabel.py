4bf8901a1a591dc76cfc7105e7fdd866b16c3c16
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-10-24 09:34:50 -0500

    Update md3_imagelabel.py

--- components/md3_imagelabel.py
+++ components/md3_imagelabel.py
@@ -26,6 +26,8 @@ class MD3ImageLabel(QtWidgets.QLabel):
             size: tuple
                 Label size
                 (w, h) -> w: width, h: height
+            scaled_image: bool
+                Fit image to label size
             theme: bool
                 App theme
                 True: Light theme, False: Dark theme
@@ -45,6 +47,8 @@ class MD3ImageLabel(QtWidgets.QLabel):
         x, y = attributes['position'] if 'position' in attributes else (8,8)
         w, h = attributes['size'] if 'size' in attributes else (96, 96)
         self.setGeometry(x, y, w, h)
+
+        self.setScaledContents(attributes['scaled_image'])
         
         self.setFrameStyle(QtWidgets.QFrame.Shape.Box)
 
@@ -62,6 +66,5 @@ class MD3ImageLabel(QtWidgets.QLabel):
         
         self.setStyleSheet(f'QLabel#{self.name} {{ '
                 f'border: 1px solid {border_color};'
-                f'border-radius: 12px;'
                 f'background-color: {background_color};'
                 f'}}' )
