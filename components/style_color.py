bc6bbe798fac8ebca2ab04ff0691e870e40ce27b
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-10-25 06:45:39 -0500

    Update style_color.py

--- components/style_color.py
+++ components/style_color.py
@@ -1,15 +1,16 @@
 def colors(theme: bool, role: str) -> str:
     light = {
-        'background': '#eceff4',
-        'on_background': '#001f2a',
-        'surface_tint': '#d8dee9',
-        'surface': '#eceff4',
-        'on_surface': '#001f2a',
+        'background': '#e5e9f0',
+        'on_background': '#000000',
 
-        'outline': '#2e3440',
+        'surface': '#b2b2b2',
+        'on_surface': '#000000',
+        'surface_tint': '#b2b2b2',
+        'outline': '#2e3441',
 
         'primary': '#5e81ac',
         'on_primary': '#2e3440',
+
         'secondary': '#81a1c1',
         'on_secondary': '#2e3440',
 
@@ -24,16 +25,17 @@ def colors(theme: bool, role: str) -> str:
     }
 
     dark = {
-        'background': '#2e3440',
-        'on_background': '#bfe9ff',
-        'surface_tint': '#4c566a',
-        'surface': '#2e3440',
-        'on_surface': '#bfe9ff',
+        'background': '#3b4253',
+        'on_background': '#e5e9f0',
 
+        'surface': '#2e3441',
+        'on_surface': '#e5e9f0',
+        'surface_tint': '#2e3441',
         'outline': '#d8dee9',
 
         'primary': '#81a1c1',
         'on_primary': '#eceff4',
+        
         'secondary': '#5e81ac',
         'on_secondary': '#eceff4',
 
