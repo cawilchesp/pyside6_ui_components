6e2dd62b4f68604f4a39fdb11a78badce94de168
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-10-30 20:59:50 -0500

    Update md3_switch.py

--- components/md3_switch.py
+++ components/md3_switch.py
@@ -7,8 +7,6 @@ PySide6 Icon Button component adapted to follow Material Design 3 guidelines
 from PySide6 import QtGui, QtWidgets, QtCore
 from PySide6.QtCore import Qt
 
-from components.style_color import colors
-
 import sys
 
 # ------
