e6eb84ef05c898cac80e4b4680b84aad408b723f
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-10-30 20:59:47 -0500

    Update md3_slider.py

--- components/md3_slider.py
+++ components/md3_slider.py
@@ -6,8 +6,6 @@ PySide6 Slider component adapted to follow Material Design 3 guidelines
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
 
-from components.style_color import colors
-
 # ------
 # Slider
 # ------
