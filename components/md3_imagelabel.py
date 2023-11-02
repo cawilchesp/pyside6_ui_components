4f6a552ac073c29dfe6b184afe3b148cc6389971
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-11-02 16:56:55 -0500

    Update md3_imagelabel.py

--- components/md3_imagelabel.py
+++ components/md3_imagelabel.py
@@ -1,23 +1,19 @@
 """
 PySide6 Video label component adapted to follow Material Design 3 guidelines
 
-
 """
-
-from PySide6 import QtWidgets
+from PySide6.QtWidgets import QLabel, QFrame
 
 # -----------
 # Image Label
 # -----------
-class MD3ImageLabel(QtWidgets.QLabel):
+class MD3ImageLabel(QLabel):
     def __init__(self, parent, attributes: dict) -> None:
         """ Material Design 3 Component: Image Label
 
         Parameters
         ----------
         attributes: dict
-            name: str
-                Widget name
             position: tuple
                 Label position
                 (x, y) -> x, y: upper left corner
@@ -26,9 +22,6 @@ class MD3ImageLabel(QtWidgets.QLabel):
                 (w, h) -> w: width, h: height
             scaled_image: bool
                 Fit image to label size
-            theme: bool
-                App theme
-                True: Light theme, False: Dark theme
         
         Returns
         -------
@@ -45,4 +38,4 @@ class MD3ImageLabel(QtWidgets.QLabel):
 
         self.setScaledContents(attributes['scaled_image'])
         
-        self.setFrameStyle(QtWidgets.QFrame.Shape.Box)
+        self.setFrameStyle(QFrame.Shape.Box)
