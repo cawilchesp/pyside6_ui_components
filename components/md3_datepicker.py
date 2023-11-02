838d78d3d67f23962a8df446b3cc14d2f921768c
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-11-02 15:27:26 -0500

    Update md3_datepicker.py

--- components/md3_datepicker.py
+++ components/md3_datepicker.py
@@ -1,41 +1,34 @@
 """
 PySide6 Date Picker component adapted to follow Material Design 3 guidelines
 
-
 """
-
-from PySide6 import QtGui, QtWidgets, QtCore
-
-import sys
+from PySide6.QtWidgets import QFrame, QDateEdit, QLabel
+from PySide6.QtGui import QFont
+from PySide6.QtCore import QDate
 
 # -----------
 # Date Picker
 # -----------
-class MD3DatePicker(QtWidgets.QFrame):
+class MD3DatePicker(QFrame):
     def __init__(self, parent, attributes: dict) -> None:
         """ Material Design 3 Component: Date Field
 
         Parameters
         ----------
         attributes: dict
-            name: str
-                Widget name
             position: tuple
-                Date field position
-                (x, y) -> x, y: upper left corner
+                Date field top left corner position
+                (x, y)
             width: int
                 Date field width
             type: str
                 Date field type
                 'filled', 'outlined'
             labels: tuple
-                Date field text
-                (label_es, label_en) -> label_es: label in spanish, label_en: label in english
+                Date field labels
+                (label_spanish, label_english)
             enabled: bool
                 Date field enabled / disabled
-            theme: bool
-                App theme
-                True: Light theme, False: Dark theme
             language: int
                 App language
                 0: Spanish, 1: English
@@ -53,18 +46,18 @@ class MD3DatePicker(QtWidgets.QFrame):
         w = attributes['width'] if 'width' in attributes else 96
         self.setGeometry(x, y, w, 52)
 
-        self.text_field = QtWidgets.QDateEdit(self)
+        self.text_field = QDateEdit(self)
         self.text_field.setGeometry(0, 8, w, 44)
         
         self.text_field.setCalendarPopup(True)
         self.text_field.setFrame(False)
         self.text_field.setSpecialValueText('')
-        self.text_field.setDate(QtCore.QDate.currentDate())
+        self.text_field.setDate(QDate.currentDate())
         
-        self.label_field = QtWidgets.QLabel(self)
+        self.label_field = QLabel(self)
         self.label_field.move(8, 0)
         self.label_field.setMargin(4)
-        self.label_field.setFont(QtGui.QFont('Segoe UI', 9))
+        self.label_field.setFont(QFont('Segoe UI', 9))
 
         self.setEnabled(attributes['enabled']) if 'enabled' in attributes else True
 
