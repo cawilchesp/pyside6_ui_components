5d2432ff8996a164ae83fe68d5def3eec3800e21
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-12-21 17:14:12 -0500

    Update md3_window.py

--- components/md3_window.py
+++ components/md3_window.py
@@ -1,64 +1,53 @@
-"""
-PySide6 Main Window
+from PySide6.QtWidgets import QWidget
 
-"""
-
-# ------
-# Window
-# ------
-class MD3Window:
-    def __init__(self, attributes):
-        """ Main Window
-
+class MD3Window(QWidget):
+    """
+    PySide6 Main Window
+    """
+    def __init__(
+        self,
+        parent: QWidget,
+        size: tuple[int, int] = (1300, 700),
+        position: tuple[int, int] = None,
+        minimum_size: tuple[int, int] = None,
+        maximum_size: tuple[int, int] = None,
+        titles: tuple[str, str] = None,
+        language: int = 0
+    ):
+        """
         Parameters
         ----------
-        attributes: dict
-            parent:
-                UI Parent object 
-            size: tuple
-                Window width and height
-                (width, height)
-            position: tuple (Optional)
-                Window position, centered by default
-                (x, y) -> x, y: upper left corner
-            minimum_size: tuple
-                minimum window size when resized
+            parent (QWidget): UI Parent object
+            size (tuple[int, int]): Window width and height (width, height)
+            position (tuple[int, int]): Window top left corner position (x, y)
+                Centered by default
+            minimum_size (tuple[int, int]): minimum window size when resized 
                 (minimum_width, minimum_height)
-            maximum_size: tuple
-                maximum window size when resized
+            maximum_size (tuple[int, int]): maximum window size when resized
                 (maximum_width, maximum_height)
-            labels: tuple
-                Window title labels
-                (label_spanish, label_english)
-            language: int
-                App language
-                0: Spanish, 1: English
-        
-        Returns
-        -------
-        None
+            titles (tuple[str, str]): Window titles (title_spanish, title_english)
+            language (int): App language
+                Options: 0 = Spanish, 1 = English
         """
-        self.parent = attributes['parent']
-        self.attributes = attributes
+        super().__init__(parent)
 
-        w, h = attributes['size'] if 'size' in attributes else (1300, 700)
-        if 'position' in attributes:
-            x, y = attributes['position']
+        self.parent = parent
+        self.parent.resize(size[0], size[1])
+        if position is None:
+            screen_x = int(self.parent.screen().availableGeometry().width() / 2 - (size[0] / 2))
+            screen_y = int(self.parent.screen().availableGeometry().height() / 2 - (size[1] / 2))
+            x, y = (screen_x, screen_y)
         else:
-            screen_x = int(self.parent.screen().availableGeometry().width() / 2 - (w / 2))
-            screen_y = int(self.parent.screen().availableGeometry().height() / 2 - (h / 2))
-            x, y = (screen_x, screen_y) 
-        self.parent.setGeometry(x, y, w, h)
-        if 'minimum_size' in attributes:
-            w_min, h_min = attributes['minimum_size']
-            self.parent.setMinimumSize(w_min, h_min)
-        if 'maximum_size' in attributes:
-            w_max, h_max = attributes['maximum_size']
-            self.parent.setMaximumSize(w_max, h_max)
-        self.set_language(attributes['language'])
+            x, y = (position[0],position[1])
+        self.parent.move(x,y)
+        self.parent.setMinimumSize(minimum_size[0], minimum_size[1]) if minimum_size is not None else None
+        self.parent.setMaximumSize(maximum_size[0], maximum_size[1]) if maximum_size is not None else None
+        self.titles = titles
+        
+        self.set_language(language) if self.titles is not None else None
 
 
     def set_language(self, language: int) -> None:
-        """ Change language of title text """
-        if language == 0:   self.parent.setWindowTitle(self.attributes['labels'][0])
-        elif language == 1: self.parent.setWindowTitle(self.attributes['labels'][1])
+        """ Change language of title window """
+        if language == 0:   self.parent.setWindowTitle(self.titles[0])
+        elif language == 1: self.parent.setWindowTitle(self.titles[1])
