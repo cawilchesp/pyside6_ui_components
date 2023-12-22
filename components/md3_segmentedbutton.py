ea49ee514406b5fa1410ab0ac8c27ab26cd47fb1
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-12-21 20:19:49 -0500

    Update md3_segmentedbutton.py

--- components/md3_segmentedbutton.py
+++ components/md3_segmentedbutton.py
@@ -1,88 +1,81 @@
-"""
-PySide6 Segmented Button component adapted to follow Material Design 3 guidelines
-
-"""
-from PySide6.QtWidgets import QToolButton
+from PySide6.QtWidgets import QToolButton, QWidget
 from PySide6.QtCore import Qt
 
 from icon_color import icon_color
 
-# ----------------
-# Segmented Button
-# ----------------
-class MD3SegmentedButton(QToolButton):
-    def __init__(self, parent, attributes: dict) -> None:
-        """ Material Design 3 Component: Segmented Button
 
+class MD3SegmentedButton(QToolButton):
+    """
+    PySide6 Segmented Button component
+    """
+    def __init__(
+        self,
+        parent: QWidget,
+        clicked_signal: callable,
+        position: tuple[int, int] = (8,8),
+        width: int = 32,
+        icon_name: str = 'none',
+        labels: tuple[str, str] = None,
+        check_icon: bool = True,
+        location: str = 'center',
+        state: bool = False,
+        enabled: bool = True,
+        theme_color: str = 'blue',
+        language: int = 0
+    ):
+        """
         Parameters
         ----------
-        attributes: dict
-            position: tuple
-                Button top left corner position
-                (x, y)
-            width: int
-                Button width
-            labels: tuple
-                Segmented button labels
-                (label_spanish, label_english)
-            icon: str (Optional)
-                Icon name
-            check_icon: bool
-                Use check icon for selected option
-            location: str
-                Position of the segmented button in the group
-                'left', 'center', 'right'
-            state: bool
-                State of activation
-                True: On, False: Off
-            enabled: bool
-                Segmented button enabled / disabled
-            theme_color: str
-                App theme color name
-            language: int
-                App language
-                0: Spanish, 1: English
-            clicked: def
-                Segmented button 'clicked' method name
-        
-        Returns
-        -------
-        None
+            parent (QWidget): UI Parent object
+            clicked (callable): Segmented button 'clicked' method name
+            position (tuple[int, int]): Button top left corner position (x, y)
+            width (int): Button width
+            icon_name (str): Icon name
+            labels (tuple[str, str]): Segmented button labels (label_spanish, label_english)
+            check_icon (bool): Use "check" icon for selected option
+            location (str): Position of the segmented button in the group
+                Options: 'left', 'center', 'right'
+            state (bool): State of activation
+                Options: True: On, False: Off
+            enabled (bool): Segmented button enabled / disabled
+            theme_color (str): App theme color name
+            language (int): App language
+                Options: 0 = Spanish, 1 = English
         """
-        super(MD3SegmentedButton, self).__init__(parent)
+        super().__init__(parent)
 
-        self.attributes = attributes
         self.parent = parent
-        x, y = attributes['position'] if 'position' in attributes else (8,8)
-        w = attributes['width'] if 'width' in attributes else 32
-        self.setGeometry(x, y, w, 32)
+        self.move(position[0], position[1])
+        self.resize(width, 32)
         self.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
         self.setCheckable(True)
-        self.setEnabled(attributes['enabled']) if 'enabled' in attributes else True
+        self.setEnabled = enabled
+        self.icon_name = icon_name
+        self.check_icon = check_icon
+        self.labels = labels
 
-        self.set_state(attributes['state'], attributes['theme_color'])
-        self.setProperty(self.attributes['location'], True)
-        self.set_language(attributes['language'])
+        self.set_state(state, theme_color)
+        self.set_language(language) if self.labels is not None else None
+        self.setProperty(location, True)
 
-        self.clicked.connect(attributes['clicked'])
+        self.clicked.connect(clicked_signal)
         
 
-    def set_state(self, state: bool, color_name: str) -> None:
+    def set_state(self, state: bool, theme_color: str) -> None:
         """ Set button state and corresponding icon """
         self.setChecked(state)
-        icon_name = self.attributes['icon'] if 'icon' in self.attributes else 'none'
+        active_icon = self.icon_name
         if state:
-            if self.attributes['check_icon']:
-                icon_name = 'done'
+            if self.check_icon: active_icon = 'done'
             color = 'black'
         else:
-            color = color_name
-        colorized_icon = icon_color(color, icon_name)
+            color = theme_color
+        colorized_icon = icon_color(color, active_icon)
         self.setIcon(colorized_icon)
 
 
     def set_language(self, language: int) -> None:
-        """ Change language of label text """
-        if 'labels' in self.attributes:
-            if language == 0:   self.setText(self.attributes['labels'][0])
-            elif language == 1: self.setText(self.attributes['labels'][1])
+        """ Change language of button label """
+        if self.labels is not None:
+            if language == 0:   self.setText(self.labels[0])
+            elif language == 1: self.setText(self.labels[1])
