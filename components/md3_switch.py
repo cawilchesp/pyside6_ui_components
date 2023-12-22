c768bd402987d9206a4199d7980bd90f23b26838
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-12-21 20:54:30 -0500

    Update md3_switch.py

--- components/md3_switch.py
+++ components/md3_switch.py
@@ -1,68 +1,59 @@
-"""
-PySide6 Icon Button component adapted to follow Material Design 3 guidelines
-
-"""
-from PySide6.QtWidgets import QFrame, QToolButton
+from PySide6.QtWidgets import QFrame, QToolButton, QWidget
 from PySide6.QtCore import Qt, QSize
 
 from icon_color import icon_color
 
-# ------
-# Switch
-# ------
-class MD3Switch(QFrame):
-    def __init__(self, parent, attributes: dict) -> None:
-        """ Material Design 3 Component: Switch
 
+class MD3Switch(QFrame):
+    """
+    PySide6 Switch component
+    """
+    def __init__(self,
+        parent: QWidget,
+        clicked_signal: callable,
+        position: tuple[int, int] = (8,8),
+        state: bool = False,
+        enabled: bool = True,
+        theme_color: str = 'blue'
+    ):
+        """
         Parameters
         ----------
-        attributes: dict
-            position: tuple
-                Button top left corner position
-                (x, y)
-            state: bool
-                State of activation
-                True: On, False: Off
-            enabled: bool
-                Switch enabled / disabled
-            theme_color: str
-                App theme color name
-            clicked: def
-                Switch 'clicked' method name
-        
-        Returns
-        -------
-        None
+            parent (QWidget): UI Parent object
+            clicked_signal (callable): Button 'clicked' method name
+            position (tuple[int, int]): Button top left corner position (x, y)
+            state (bool): State of activation
+                Options: True: On, False: Off
+            enabled (bool): Button enabled / disabled
+            theme_color (str): App theme color name
         """
-        super(MD3Switch, self).__init__(parent)
+        super().__init__(parent)
 
-        self.attributes = attributes
         self.parent = parent
-
-        x, y = attributes['position'] if 'position' in attributes else (0, 0)
-        self.setGeometry(x, y, 52, 32)
+        self.move(position[0], position[1])
+        self.resize(52, 32)
 
         self.left_switch = QToolButton(self)
         self.left_switch.setGeometry(0, 0, 26, 32)
         self.left_switch.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonIconOnly)
         self.left_switch.setCheckable(True)
-        self.left_switch.setEnabled(attributes['enabled']) if 'enabled' in attributes else True
+        self.left_switch.setEnabled = enabled
 
         self.right_switch = QToolButton(self)
         self.right_switch.setGeometry(26, 0, 26, 32)
         self.right_switch.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonIconOnly)
         self.right_switch.setCheckable(True)
-        self.right_switch.setEnabled(attributes['enabled']) if 'enabled' in attributes else True
+        self.right_switch.setEnabled = enabled
 
         self.left_switch.setProperty('left', True)
         self.right_switch.setProperty('right', True)
-        self.set_state(attributes['state'], attributes['theme_color'])
+        self.set_state(state, theme_color)
 
-        self.left_switch.clicked.connect(attributes['clicked'])
-        self.right_switch.clicked.connect(attributes['clicked'])
+        self.left_switch.clicked.connect(clicked_signal)
+        self.right_switch.clicked.connect(clicked_signal)
         
 
-    def set_state(self, state: bool, color_name: str) -> None:
+    def set_state(self, state: bool, theme_color: str) -> None:
         """ Set button state and corresponding icon """
         
         self.left_switch.setChecked(state)
@@ -73,7 +64,7 @@ class MD3Switch(QFrame):
         }
         icon_left_name = icon_state[state]['left']
         icon_right_name = icon_state[state]['right']
-        color = 'black' if state else color_name
+        color = 'black' if state else theme_color
         colorized_left_icon = icon_color(color, icon_left_name)
         colorized_right_icon = icon_color(color, icon_right_name)
         self.left_switch.setIcon(colorized_left_icon)
