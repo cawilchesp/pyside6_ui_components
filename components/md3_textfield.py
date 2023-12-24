473dcc2874af412576ea1f4e7e1dc04702011cd6
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-12-23 20:44:48 -0500

    Update md3_textfield.py

--- components/md3_textfield.py
+++ components/md3_textfield.py
@@ -14,15 +14,28 @@ class MD3TextField(QFrame):
         parent: QWidget,
         position: tuple[int, int] = (8,8),
         width: int = 96,
+        type: str = 'filled',
+        input: str = None,
+        length: int = 32767,
+        labels: tuple[str, str] = None,
+        enabled: bool = True,
+        theme_color: str = 'blue',
+        language: int = 0,
+        return_pressed_signal: callable = None,
+        text_edited_signal: callable = None,
+        text_changed_signal: callable = None
     ):
         """
         Parameters
         ----------
+            parent (QWidget): UI Parent object
+            return_pressed_signal (callable): Text field 'return pressed' method name
+            text_edited_signal (callable): Text field 'text edited' method name
+            text_changed_signal (callable): Text field 'text changed' method name
             position (tuple[int, int]): Text field top left corner position (x, y)
             width (int): Text field width
-            type: str
-                Text field type
-                'filled', 'outlined'
+            type (str): Text field type
+                Options: 'filled', 'outlined'
             input: str (optional, any character allowed if not specified)
                 Text field type
                     'text':      only letters and accents
@@ -34,38 +47,28 @@ class MD3TextField(QFrame):
                     'email':     text in email format
                     'ip':        numbers in ip format (0.0.0.0 - 255.255.255.255)
                     'password':  any character with visible/hidden icon
-            width: int
-                Text field width
-            length: int
-                Number of characters allowed
-            labels: tuple
-                Text field labels
-                (label_spanish, label_english)
-            enabled: bool
-                Text field enabled / disabled
-            theme_color: str
-                App theme color name
-            language: int
-                App language
-                0: Spanish, 1: English
-            return_pressed: def
-                Text field 'return pressed' method name
-            text_edited: def
-                Text field 'text edited' method name
-            text_changed: def
-                Text field 'text changed' method name
+            length (int): Number of characters allowed
+            labels (tuple[str, str]): Text field labels (label_spanish, label_english)
+            enabled (bool): Button enabled / disabled
+            theme_color (str): App theme color name
+            language (int): App language
+                Options: 0 = Spanish, 1 = English
         """
         super().__init__(parent)
 
         self.parent = parent
         self.move(position[0], position[1])
         self.resize(width, 52)
+        self.setEnabled = enabled
+        self.type = type
+        self.labels = labels
 
         self.text_field = QLineEdit(self)
         self.text_field.setGeometry(0, 8, width, 44)
         self.text_field.setClearButtonEnabled(True)
+        self.text_field.setMaxLength(length)
 
-        if 'input' in attributes:
+        if input is not None:
             patterns_dict = {
                 'text': r"[\p{L}\s]+",
                 'integer': r'[+-]?\d+',
@@ -76,22 +79,18 @@ class MD3TextField(QFrame):
                 'email': r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+',
                 'ip': r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
             }
-            if attributes['input'] in patterns_dict:
-                pattern = patterns_dict[attributes['input']]
-                reg_exp = QRegularExpressionValidator(QRegularExpression(f'{pattern}'))
+            if input in patterns_dict:
+                pattern = patterns_dict[input]
+                reg_exp = QRegularExpressionValidator(QRegularExpression(pattern))
                 self.text_field.setValidator(reg_exp)
-
-            if attributes['input'] == 'password':
-                self.visible_icon = icon_color(attributes['theme_color'], 'eye')
-                self.hidden_icon = icon_color(attributes['theme_color'], 'eye_off')
+            elif input == 'password':
+                self.visible_icon = icon_color(theme_color, 'eye')
+                self.hidden_icon = icon_color(theme_color, 'eye_off')
                                 
                 self.text_field.setEchoMode(QLineEdit.EchoMode.Password)
                 self.toggle_password = self.text_field.addAction(self.visible_icon, QLineEdit.ActionPosition.TrailingPosition)
                 self.toggle_password.triggered.connect(self.password_action)
                 self.toggle_password_state = False
-
-        if 'length' in attributes:
-            self.text_field.setMaxLength(attributes['length'])
 
         self.label_field = QLabel(self)
         self.label_field.move(8, 0)
@@ -99,17 +98,12 @@ class MD3TextField(QFrame):
         self.label_field.setFont(QFont('Segoe UI', 9))
         self.label_field.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignBottom)
 
-        self.setEnabled(attributes['enabled']) if 'enabled' in attributes else True
+        self.set_language(language) if self.labels is not None else None
+        self.setProperty(self.type, True)
 
-        self.setProperty(attributes['type'], True)
-        self.set_language(attributes['language'])
-
-        if 'return_pressed' in attributes:
-            self.text_field.returnPressed.connect(attributes['return_pressed'])
-        if 'text_edited' in attributes:
-            self.text_field.textEdited.connect(attributes['text_edited'])
-        if 'text_changed' in attributes:
-            self.text_field.textChanged.connect(attributes['text_changed'])
+        self.text_field.returnPressed.connect(return_pressed_signal)
+        self.text_field.textEdited.connect(text_edited_signal)
+        self.text_field.textChanged.connect(text_changed_signal)
           
 
     def password_action(self) -> None:
@@ -125,7 +119,7 @@ class MD3TextField(QFrame):
 
     def set_language(self, language: int) -> None:
         """ Change language of label text """
-        if language == 0:   self.label_field.setText(self.attributes['labels'][0])
-        elif language == 1: self.label_field.setText(self.attributes['labels'][1])
+        if language == 0:   self.label_field.setText(self.labels[0])
+        elif language == 1: self.label_field.setText(self.labels[1])
         self.label_field.adjustSize()
         self.label_field.resize(self.label_field.width(), 20)
