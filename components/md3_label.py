a8893492a4560444d538bc9d8d9fb411f3eefec4
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-12-28 11:22:46 -0500

    Update md3_label.py

--- components/md3_label.py
+++ components/md3_label.py
@@ -1,36 +1,38 @@
-"""
-PySide6 Label component adapted to follow Material Design 3 guidelines
-
-"""
-from PySide6.QtWidgets import QLabel
+from PySide6.QtWidgets import QLabel, QWidget
 from PySide6.QtCore import Qt
 
 from icon_color import icon_color
 
-# ------
-# Labels
-# ------
-class MD3Label(QLabel):
-    def __init__(self, parent, attributes: dict) -> None:
-        """ Material Design 3 Component: Label
 
+class MD3Label(QLabel):
+    """
+    PySide6 Label component
+    """
+    def __init__(
+        self,
+        parent: QWidget,
+        position: tuple[int, int] = (8,8),
+        width: int = 32,
+        type: str = 'filled',
+        align: str = 'left',
+        icon_name: str = None,
+        labels: tuple[str, str] = None,
+        enabled: bool = True,
+        theme_color: str = 'blue',
+        language: int = 0
+    ):
+        """
         Parameters
         ----------
-        attributes: dict
-            position: tuple
-                Label top left corner position
-                (x, y)
-            width: int
-                Label width
-            type: str
-                Label type
-                For text: 'subtitle', 'value'
-                For indicators: 'icon', 'color'
-            align: str 
-                Text align
-                'center', 'left', 'right'
-            icon: str
-                Icon name
+            parent (QWidget): UI Parent object
+            position (tuple[int, int]): Label top left corner position (x, y)
+            width (int): Label width
+            type (str): Label type
+                Text options: 'subtitle', 'value'
+                Indicator options: 'icon', 'color'
+            align (str): Text align
+                Options: 'center', 'left', 'right'
+            icon_name (str): Icon name
             color: str
                 Label color in hexadecimal format
                 '#RRGGBB'
@@ -50,26 +52,24 @@ class MD3Label(QLabel):
         -------
         None
         """
-        super(MD3Label, self).__init__(parent)
+        super().__init__(parent)
 
-        self.attributes = attributes
         self.parent = parent
+        self.move(position[0], position[1])
+        height = 16 if type == 'subtitle' else 32
+        self.resize(width, height)
 
-        x, y = attributes['position'] if 'position' in attributes else (8,8)
-        w = attributes['width'] if 'width' in attributes else 32
-        h = 16 if attributes['type'] == 'subtitle' else 32
-        self.setGeometry(x, y, w, h)
-
-        if attributes['type'] in {'subtitle', 'value'}:
+        if type in {'subtitle', 'value'}:
             alignment_dict = {
                 'left': Qt.AlignmentFlag.AlignLeft,
                 'center': Qt.AlignmentFlag.AlignHCenter,
                 'right': Qt.AlignmentFlag.AlignRight
             }
-            label_alignment = alignment_dict[attributes['align']]
-            self.setAlignment(label_alignment | Qt.AlignmentFlag.AlignVCenter)
+            label_H_alignment = alignment_dict[align]
+            self.setAlignment(label_H_alignment | Qt.AlignmentFlag.AlignVCenter)
 
-        self.setProperty(attributes['type'], True)
+        self.setProperty(type, True)
+        
         if attributes['type'] == 'value':
             self.setStyleSheet(f"MD3Label[value=true] {{ border-color: {attributes['border_color']} }}")
         if attributes['type'] == 'color':
