132f53a1efdd28bf9ccc732e2f3cfe3f5c5de83c
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-12-28 10:59:57 -0500

    Update md3_datepicker.py

--- components/md3_datepicker.py
+++ components/md3_datepicker.py
@@ -1,53 +1,47 @@
-"""
-PySide6 Date Picker component adapted to follow Material Design 3 guidelines
-
-"""
-from PySide6.QtWidgets import QFrame, QDateEdit, QLabel
+from PySide6.QtWidgets import QFrame, QDateEdit, QLabel, QWidget
 from PySide6.QtGui import QFont
 from PySide6.QtCore import QDate
 
-# -----------
-# Date Picker
-# -----------
-class MD3DatePicker(QFrame):
-    def __init__(self, parent, attributes: dict) -> None:
-        """ Material Design 3 Component: Date Field
 
+class MD3DatePicker(QFrame):
+    """
+    PySide6 Date Picker component
+    """
+    def __init__(
+        self,
+        parent: QWidget,
+        position: tuple[int, int] = (8,8),
+        width: int = 96,
+        type: str = 'filled',
+        labels: tuple[str, str] = None,
+        enabled: bool = True,
+        language: int = 0
+    ):
+        """
         Parameters
         ----------
-        attributes: dict
-            position: tuple
-                Date field top left corner position
-                (x, y)
-            width: int
-                Date field width
-            type: str
-                Date field type
-                'filled', 'outlined'
+            parent (QWidget): UI Parent object
+            position (tuple[int, int]): Date picker top left corner position (x, y)
+            width (int): Date picker width
+            type (str): Date picker type
+                Options: 'filled', 'outlined'
             labels: tuple
                 Date field labels
                 (label_spanish, label_english)
-            enabled: bool
-                Date field enabled / disabled
-            language: int
-                App language
-                0: Spanish, 1: English
-        
-        Returns
-        -------
-        None
+            enabled (bool): Date picker enabled / disabled
+            language (int): App language
+                Options: 0 = Spanish, 1 = English
         """
-        super(MD3DatePicker, self).__init__(parent)
+        super().__init__(parent)
 
-        self.attributes = attributes
         self.parent = parent
-
-        x, y = attributes['position'] if 'position' in attributes else (8,8)
-        w = attributes['width'] if 'width' in attributes else 96
-        self.setGeometry(x, y, w, 52)
+        self.move(position[0], position[1])
+        self.resize(width, 52)
+        self.setEnabled = enabled
+        self.labels = labels
 
         self.text_field = QDateEdit(self)
-        self.text_field.setGeometry(0, 8, w, 44)
+        self.text_field.setGeometry(0, 8, width, 44)
         
         self.text_field.setCalendarPopup(True)
         self.text_field.setFrame(False)
@@ -59,15 +53,13 @@ class MD3DatePicker(QFrame):
         self.label_field.setMargin(4)
         self.label_field.setFont(QFont('Segoe UI', 9))
 
-        self.setEnabled(attributes['enabled']) if 'enabled' in attributes else True
-
-        self.setProperty(attributes['type'], True)
-        self.set_language(attributes['language'])
+        self.set_language(language) if self.labels is not None else None
+        self.setProperty(type, True)
 
 
     def set_language(self, language: int) -> None:
-        """ Change language of label text """
-        if language == 0:   self.label_field.setText(self.attributes['labels'][0])
-        elif language == 1: self.label_field.setText(self.attributes['labels'][1])
+        """ Change language of date picker text """
+        if language == 0:   self.label_field.setText(self.labels[0])
+        elif language == 1: self.label_field.setText(self.labels[1])
         self.label_field.adjustSize()
         self.label_field.resize(self.label_field.width(), 20)
