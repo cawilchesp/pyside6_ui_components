6568fa12c61070c05dbdbeff6a96ff4dac7508fc
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-01-02 12:10:41 -0500

    Update md3_imagelabel.py

--- components/md3_imagelabel.py
+++ components/md3_imagelabel.py
@@ -1,41 +1,30 @@
-"""
-PySide6 Video label component adapted to follow Material Design 3 guidelines
+from PySide6.QtWidgets import QLabel, QFrame, QWidget
 
-"""
-from PySide6.QtWidgets import QLabel, QFrame
 
-# -----------
-# Image Label
-# -----------
 class MD3ImageLabel(QLabel):
-    def __init__(self, parent, attributes: dict) -> None:
-        """ Material Design 3 Component: Image Label
-
+    """
+    PySide6 Image Label component
+    """
+    def __init__(
+        self,
+        parent: QWidget,
+        position: tuple[int, int] = (8,8),
+        size: tuple[int, int] = (96,96),
+        scaled_image: bool = True,
+    ):
+        """
         Parameters
         ----------
-        attributes: dict
-            position: tuple
-                Label position
-                (x, y) -> x, y: upper left corner
-            size: tuple
-                Label size
-                (w, h) -> w: width, h: height
-            scaled_image: bool
-                Fit image to label size
-        
-        Returns
-        -------
-        None
+            position (tuple[int, int]): Image label top left corner position (x, y)
+            size (tuple[int, int]): Image label size (width, height)
+            scaled_image (bool): Fit image to label
+                Options: True: Fit, False: Unfit
         """
-        super(MD3ImageLabel, self).__init__(parent)
+        super().__init__(parent)
 
-        self.attributes = attributes
         self.parent = parent
+        self.move(position[0], position[1])
+        self.resize(size[0], size[1])
 
-        x, y = attributes['position'] if 'position' in attributes else (8,8)
-        w, h = attributes['size'] if 'size' in attributes else (96, 96)
-        self.setGeometry(x, y, w, h)
-
-        self.setScaledContents(attributes['scaled_image'])
-        
+        self.setScaledContents(scaled_image)
         self.setFrameStyle(QFrame.Shape.Box)
