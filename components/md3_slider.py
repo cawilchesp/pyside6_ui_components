f6699cc090016260ded69138e2194f41d6fe6c61
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-01-02 11:55:32 -0500

    Update md3_slider.py

--- components/md3_slider.py
+++ components/md3_slider.py
@@ -1,58 +1,53 @@
-"""
-PySide6 Slider component adapted to follow Material Design 3 guidelines
-
-"""
-from PySide6.QtWidgets import QSlider
+from PySide6.QtWidgets import QSlider, QWidget
 from PySide6.QtCore import Qt
 
-# ------
-# Slider
-# ------
-class MD3Slider(QSlider):
-    def __init__(self, parent, attributes: dict) -> None:
-        """ Material Design 3 Component: Slider
 
+class MD3Slider(QSlider):
+    """
+    PySide6 Slider component
+    """
+    def __init__(
+        self,
+        parent: QWidget,
+        slider_moved_signal: callable,
+        slider_released_signal: callable,
+        position: tuple[int, int] = (8,8),
+        length: int = 32,
+        orientation: str = 'horizontal',
+        range: tuple[int, int, int] = (0, 1, 10),
+        value: int = 0,
+        enabled: bool = True
+    ):
+        """
         Parameters
         ----------
-        attributes: dict
-            position: tuple
-                Slider top left corner position
-                (x, y)
-            width: int
-                Slider width
-            range: tuple
-                Slider range (min, step, max)
-            value: int
-                Slider current value
-            enabled: bool
-                Slider enabled / disabled
-            slider_moved: def
-                Slider 'moved' method name
-            slider_released: def
-                Slider 'released' method name
-        
-        Returns
-        -------
-        None
+            parent (QWidget): UI Parent object
+            slider_moved_signal (callable): Slider 'moved' method name
+            slider_released_signal (callable): Slider 'released' method name
+            position (tuple[int, int]): Slider top left corner position (x, y)
+            length (int): Slider length
+            orientation (str): Slider orientation
+                Options: 'horizontal', 'vertical'
+            range (tuple[int, int, int]): Slider range (min, step, max)
+            value (int): Slider current value
+            enabled (bool): Button enabled / disabled
         """
-        super(MD3Slider, self).__init__(parent)
+        super().__init__(parent)
 
-        self.attributes = attributes
         self.parent = parent
+        self.move(position[0], position[1])
+        if orientation == 'horizontal':
+            self.resize(length, 32)
+            self.setOrientation(Qt.Orientation.Horizontal)
+        elif orientation == 'vertical':
+            self.resize(32, length)
+            self.setOrientation(Qt.Orientation.Vertical)
 
-        x, y = attributes['position'] if 'position' in attributes else (8,8)
-        w = attributes['width'] if 'width' in attributes else 32
-        self.setGeometry(x, y, w, 32)
+        self.setMinimum(range[0])
+        self.setSingleStep(range[1])
+        self.setMaximum(range[2])
+        self.setValue(value)
+        self.setEnabled = enabled
 
-        self.setOrientation(Qt.Orientation.Horizontal)
-        self.setMinimum(attributes['range'][0])
-        self.setSingleStep(attributes['range'][1])
-        self.setMaximum(attributes['range'][2])
-
-        self.setValue(attributes['value'])
-        self.setEnabled(attributes['enabled']) if 'enabled' in attributes else True
-        
-        if 'slider_moved' in attributes:
-            self.sliderMoved.connect(attributes['slider_moved'])
-        if 'slider_released' in attributes:
-            self.sliderReleased.connect(attributes['slider_released'])
+        self.sliderMoved.connect(slider_moved_signal)
+        self.sliderReleased.connect(slider_released_signal)
