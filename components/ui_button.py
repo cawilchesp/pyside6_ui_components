2f2192684595e4065e77f896eb950573556d88d9
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-01-03 16:12:42 -0500

    Update ui_button.py

--- components/ui_button.py
+++ components/ui_button.py
@@ -6,7 +6,7 @@ from icon_color import icon_color
 
 class UI_Button(QToolButton):
     """
-    PySide6 Button component
+    Button component
     """
     def __init__(
         self,
@@ -19,7 +19,8 @@ class UI_Button(QToolButton):
         labels: tuple[str, str] = None,
         enabled: bool = True,
         theme_color: str = 'blue',
-        language: int = 0
+        theme_style: bool = True,
+        language: str = 'es'
     ):
         """
         Parameters
@@ -34,8 +35,9 @@ class UI_Button(QToolButton):
             labels (tuple[str, str]): Button labels (label_spanish, label_english)
             enabled (bool): Button enabled / disabled
             theme_color (str): App theme color name
+            theme_style (bool): App theme style name
             language (int): App language
-                Options: 0 = Spanish, 1 = English
+                Options: 'es' = Español, 'en' = English
         """
         super().__init__(parent)
 
@@ -43,23 +45,21 @@ class UI_Button(QToolButton):
         self.move(position[0], position[1])
         self.resize(width, 32)
         self.setEnabled(enabled)
+        self.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
         self.type = type
         self.labels = labels
 
-        self.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
-        self.setAutoRaise(True)
-        
-        self.set_icon(theme_color, icon_name) if icon_name is not None else None
+        self.set_icon(theme_style, theme_color, icon_name) if icon_name is not None else None
         self.set_language(language) if self.labels is not None else None
         self.setProperty('type', self.type)
 
         self.clicked.connect(clicked_signal)
 
 
-    def set_icon( self, theme_color: str, icon_name: str) -> None:
+    def set_icon(self, theme_style: bool, theme_color: str, icon_name: str) -> None:
         """ Change button icon """
         if self.type == 'standard':
-            color = 'white'
+            color = 'black' if theme_style else 'white'
         elif self.type == 'accent':
             color = 'black'
         elif self.type in ['outlined', 'hyperlink']:
@@ -71,5 +71,113 @@ class UI_Button(QToolButton):
     def set_language(self, language: int) -> None:
         """ Change language of button label """
         if self.labels is not None:
-            if language == 0:   self.setText(self.labels[0])
-            elif language == 1: self.setText(self.labels[1])
+            if language == 'es':   self.setText(self.labels[0])
+            elif language == 'en': self.setText(self.labels[1])
+
+
+class UI_ToggleButton(QToolButton):
+    """
+    Toggle Button component
+    """
+    def __init__(
+        self,
+        parent: QWidget,
+        clicked_signal: callable,
+        position: tuple[int, int] = (8,8),
+        width: int = 32,
+        icon_name: str = None,
+        labels: tuple[str, str] = None,
+        state: bool = False,
+        enabled: bool = True,
+        theme_style: bool = True,
+        language: str = 'es'
+    ):
+        """
+        Parameters
+        ----------
+            parent (QWidget): UI Parent object
+            triggered_signal (callable): Button 'clicked' method name
+            position (tuple[int, int]): Button top left corner position (x, y)
+            width (int): Button width
+            icon_name (str): Icon name
+            labels (tuple[str, str]): Button labels (label_spanish, label_english)
+            state (bool): Button toggle State of activation
+                Options: True: On, False: Off
+            enabled (bool): Button enabled / disabled
+            theme_style (bool): App theme style name
+            language (int): App language
+                Options: 'es' = Español, 'en' = English
+        """
+        super().__init__(parent)
+
+        self.parent = parent
+        self.move(position[0], position[1])
+        self.resize(width, 32)
+        self.setEnabled(enabled)
+        self.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
+        self.setCheckable(True)
+        self.icon_name = icon_name
+        self.labels = labels
+        
+        self.set_icon(state, theme_style) if icon_name is not None else None
+        self.set_language(language) if self.labels is not None else None
+
+        self.clicked.connect(clicked_signal)
+
+
+    def set_icon(self, state: bool, theme_style: bool) -> None:
+        """ Change button icon """
+        if state:
+            color = 'black'
+        else:
+            color = 'black' if theme_style else 'white'
+        colorized_icon = icon_color(color, self.icon_name)
+        self.setIcon(colorized_icon)
+
+
+    def set_language(self, language: int) -> None:
+        """ Change language of button label """
+        if self.labels is not None:
+            if language == 'es':   self.setText(self.labels[0])
+            elif language == 'en': self.setText(self.labels[1])
+
+
+class UI_ThemeButton(QToolButton):
+    """
+    Theme Button component
+    """
+    def __init__(self,
+        parent: QWidget,
+        clicked_signal: callable,
+        position: tuple[int, int] = (8,8),
+        state: bool = False,
+        enabled: bool = True
+    ):
+        """
+        Parameters
+        ----------
+            parent (QWidget): UI Parent object
+            clicked_signal (callable): Button 'clicked' method name
+            position (tuple[int, int]): Button top left corner position (x, y)
+            state (bool): State of activation
+                Options: True: On, False: Off
+            enabled (bool): Button enabled / disabled
+        """
+        super().__init__(parent)
+
+        self.parent = parent
+        self.move(position[0], position[1])
+        self.resize(32, 32)
+        self.setEnabled(enabled)
+        
+        self.set_state(state)
+
+        self.clicked.connect(clicked_signal)
+
+
+    def set_state(self, state: bool) -> None:
+        """ Set button state and corresponding icon """
+        icon_name = 'light_mode' if state else 'dark_mode'
+        color = 'black'
+        colorized_icon = icon_color(color, icon_name)
+        self.setIcon(colorized_icon)
