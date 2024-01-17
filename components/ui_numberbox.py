f5deb56fece9d6c7ba05ce3a79de7fc621bf31d1
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-01-17 09:55:42 -0500

    Update ui_numberbox.py

--- components/ui_numberbox.py
+++ components/ui_numberbox.py
@@ -10,29 +10,31 @@ class UI_NumberBox(QSpinBox):
         parent: QWidget,
         position: tuple[int, int] = (8, 8),
         width: int = 64,
+        range: tuple[int, int, int] = (0, 1, 100),
+        value: int = 0,
+        enabled: bool = True
     ):
         """
         Parameters
         ----------
             parent (QWidget): UI Parent object
-            position (tuple[int, int]): Text box top left corner position (x, y)
-            width (int): Text box width
-            placeholder_texts (tuple[str, str]): Text box placeholder texts (text_spanish, text_english)
-            input (str): Characters allowed
-                Options:
-                    'alphabet': letters and accents
-                    'numeric': numbers
-                    'alphanumeric': letters and numbers
-            max_length (int): Number of characters allowed
-            enabled (bool): Text box enabled / disabled
-            language (str): App language
-                Options: 'es' = Español, 'en' = English
+            position (tuple[int, int]): Number box top left corner position (x, y)
+            width (int): Number box width
+            range (tuple[int, int, int]): Number box range (min, step, max)
+            value (int): Number box current value
+            enabled (bool): Number box enabled / disabled
         """
         super().__init__(parent)
 
         self.parent = parent
         self.move(position[0], position[1])
         self.resize(width, 40)
+        self.setEnabled(enabled)
+
+        self.setMinimum(range[0])
+        self.setSingleStep(range[1])
+        self.setMaximum(range[2])
+        self.setValue(value)
 
 
 class UI_FloatBox(QDoubleSpinBox):
@@ -42,26 +44,28 @@ class UI_FloatBox(QDoubleSpinBox):
         parent: QWidget,
         position: tuple[int, int] = (8, 8),
         width: int = 64,
+        range: tuple[float, float, float] = (0, 0.1, 1),
+        value: float = 0.0,
+        enabled: bool = True
     ):
         """
         Parameters
         ----------
             parent (QWidget): UI Parent object
-            position (tuple[int, int]): Text box top left corner position (x, y)
-            width (int): Text box width
-            placeholder_texts (tuple[str, str]): Text box placeholder texts (text_spanish, text_english)
-            input (str): Characters allowed
-                Options:
-                    'alphabet': letters and accents
-                    'numeric': numbers
-                    'alphanumeric': letters and numbers
-            max_length (int): Number of characters allowed
-            enabled (bool): Text box enabled / disabled
-            language (str): App language
-                Options: 'es' = Español, 'en' = English
+            position (tuple[int, int]): Float box top left corner position (x, y)
+            width (int): Float box width
+            range (tuple[float, float, float]): Float box range (min, step, max)
+            value (float): Float box current value
+            enabled (bool): Float box enabled / disabled
         """
         super().__init__(parent)
 
         self.parent = parent
         self.move(position[0], position[1])
         self.resize(width, 40)
+        self.setEnabled(enabled)
+
+        self.setMinimum(range[0])
+        self.setSingleStep(range[1])
+        self.setMaximum(range[2])
+        self.setValue(value)
