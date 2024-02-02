aa844bab861c9818c8f8462bedd8f9657dab7d64
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-02-02 09:41:45 -0500

    Create colors.py

---
+++ themes/colors.py
@@ -0,0 +1,30 @@
+light_colors = {
+    '@background_full': 'hsl(0, 0%, 98%)',
+    '@background_card': 'hsl(0, 0%, 90%)',
+    '@background_widget': 'hsl(0, 0%, 93%)',
+    '@background_hover': 'hsl(0, 0%, 85%)',
+    '@background_disable': 'hsl(0, 0%, 71%)',
+    '@text_active': 'hsl(0, 0%, 13%)',
+    '@text_disable': 'hsl(0, 0%, 35%)',
+}
+
+dark_colors = {
+    '@background_full': 'hsl(0, 0%, 13%)',
+    '@background_card': 'hsl(0, 0%, 17%)',
+    '@background_widget': 'hsl(0, 0%, 20%)',
+    '@background_hover': 'hsl(0, 0%, 25%)',
+    '@background_disable': 'hsl(0, 0%, 16%)',
+    '@text_active': 'hsl(0, 0%, 98%)',
+    '@text_disable': 'hsl(0, 0%, 35%)',
+}
+
+theme_colors = {
+    'blue': {
+        '@theme_active': 'hsl(200, 100%, 50%)',
+        '@theme_hover': 'hsl(200, 100%, 40%)',
+    },
+    'red': {
+        '@theme_active': 'hsl(0, 70%, 50%)',
+        '@theme_hover': 'hsl(0, 70%, 40%)',
+    }
+}
