a9c84f2beccd0218ea30af4c10687413713daf51
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-02-02 11:24:55 -0500

    Update colors.py

--- themes/colors.py
+++ themes/colors.py
@@ -3,6 +3,8 @@ light_colors = {
     '@background_card': 'hsl(0, 0%, 90%)',
     '@background_widget': 'hsl(0, 0%, 93%)',
     '@background_hover': 'hsl(0, 0%, 85%)',
+    '@border_handle': 'hsl(0, 0%, 30%)',
+    '@slider_page': 'hsl(0, 0%, 60%)',
     '@background_disable': 'hsl(0, 0%, 71%)',
     '@text_active': 'hsl(0, 0%, 13%)',
     '@text_disable': 'hsl(0, 0%, 35%)',
@@ -13,6 +15,8 @@ dark_colors = {
     '@background_card': 'hsl(0, 0%, 17%)',
     '@background_widget': 'hsl(0, 0%, 20%)',
     '@background_hover': 'hsl(0, 0%, 25%)',
+    '@border_handle': 'hsl(0, 0%, 30%)',
+    '@slider_page': 'hsl(0, 0%, 60%)',
     '@background_disable': 'hsl(0, 0%, 16%)',
     '@text_active': 'hsl(0, 0%, 98%)',
     '@text_disable': 'hsl(0, 0%, 35%)',
@@ -26,5 +30,20 @@ theme_colors = {
     'red': {
         '@theme_active': 'hsl(0, 70%, 50%)',
         '@theme_hover': 'hsl(0, 70%, 40%)',
+    }
+}
+
+icons = {
+    True: {
+        '@chevron_down': 'chevron_down_black.png',
+        '@chevron_up': 'chevron_up_black.png',
+        '@done': 'done_white.png',
+        '@minus': 'minus_white.png',
+    },
+    False: {
+        '@chevron_down': 'chevron_down_white.png',
+        '@chevron_up': 'chevron_up_white.png',
+        '@done': 'done_black.png',
+        '@minus': 'minus_black.png',
     }
 }
